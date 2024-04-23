8ca04b5a1e896d64d32898b79560c14a97cfe54c
BG270 <BG270@DESKTOP-KGTGMJJ>
2024-04-23 16:45:31 +0800

    update 29

--- src/Structure/BST.java
+++ src/Structure/BST.java
@@ -1,9 +1,99 @@
 package Structure;
 
+import Interface.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二叉搜索树
  * @Author zlx
  * @Date 2024/4/23 14:37
  */
 public class BST {
+
+    /**
+     * 头节点
+      */
+    private TreeNode root;
+
+    public void insert(int val) {
+        root = insertNode(root, val);
+    }
+
+    private TreeNode insertNode(TreeNode node, int val){
+        if (node == null) {
+            return new TreeNode(val);
+        }
+        if (val > node.val) {
+            node.right = insertNode(node.right, val);
+        }else {
+            node.left = insertNode(node.left, val);
+        }
+        return node;
+    }
+    public List<Integer> inorderTraversal() {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node != null) {
+            postorder(node.left, result);
+            result.add(node.val);
+            postorder(node.right, result);
+        }
+    }
+
+    public List<Integer> preorderTraversal() {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        return result;
+    }
+
+    private void preorder(TreeNode node, List<Integer> result) {
+        if (node != null) {
+            result.add(node.val);
+            postorder(node.left, result);
+            postorder(node.right, result);
+        }
+    }
+
+    public List<Integer> postorderTraversal() {
+        List<Integer> result = new ArrayList<>();
+        postorder(root, result);
+        return result;
+    }
+
+    private void postorder(TreeNode node, List<Integer> result) {
+        if (node != null) {
+            postorder(node.left, result);
+            postorder(node.right, result);
+            result.add(node.val);
+        }
+    }
+
+
+    public TreeNode buildTree(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            insert(nums[i]);
+        }
+        return root;
+    }
+
+    public TreeNode buildTree(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            insert(nums.get(i));
+        }
+        return root;
+    }
+
+    public int[] convertToArray(List<Integer> list) {
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
 }
--- src/TargetOffer/Solution29.java
+++ src/TargetOffer/Solution29.java
@@ -1,8 +1,74 @@
 package TargetOffer;
 
+import Structure.BST;
+
+import java.util.Arrays;
+
 /**
  * @Author zlx
  * @Date 2024/4/23 14:05
  */
 public class Solution29 {
+    public static void main(String[] args) {
+        BST bst = new BST();
+        int[] arr = {5,9,8,1,4,7,3,6,2};
+        bst.buildTree(arr);
+        int[] sequential_sequence = bst.convertToArray(bst.postorderTraversal());
+        System.out.println(verifyPostorder(sequential_sequence));
+        sequential_sequence = bst.convertToArray(bst.preorderTraversal());
+        System.out.println(verifyPostorder(sequential_sequence));
+        sequential_sequence = bst.convertToArray(bst.inorderTraversal());
+        System.out.println(verifyPostorder(sequential_sequence));
+        sequential_sequence = new  int[]{7,6,5,4};
+        System.out.println(verifyPostorder(sequential_sequence));
+    }
+
+//    public static boolean verifyPostorder(int [] sequence){
+//        if(sequence == null || sequence.length <= 0){
+//            return false;
+//        }
+//        int len = sequence.length;
+//        int root = sequence[len - 1];
+//        int i = 0;
+//        for(; i < len - 1; i++){
+//            if(root <= sequence[i]){
+//                break;
+//            }
+//        }
+//        int j = i;
+//        for(; j < len - 1; j++){
+//            if(root > sequence[j]){
+//                return false;
+//            }
+//        }
+//        boolean leftFlag = true;
+//        if (i > 0){
+//            leftFlag = verifyPostorder(Arrays.copyOfRange(sequence, 0, i));
+//        }
+//        boolean rightFlag = true;
+//        if(i < len - 1){
+//            rightFlag = verifyPostorder(Arrays.copyOfRange(sequence, i, sequence.length - 1));
+//        }
+//        return leftFlag && rightFlag;
+//    }
+
+    public static boolean verifyPostorder(int[] postorder) {
+        if (postorder == null || postorder.length == 0) return false;
+        return divideAndConquer(postorder, 0 , postorder.length - 1);
+    }
+
+    private static boolean divideAndConquer(int[] postorder, int start, int end) {
+        if (start >= end) return true;
+        int root = postorder[end];
+        int l ;
+        for (l = start; l < end; l++) {
+            if (postorder[l] > root) break;
+        }
+        for (int i = l; i < end; i++) {
+            if (postorder[i] < root) return false;
+        }
+        boolean left = divideAndConquer(postorder, start, l - 1);
+        boolean right = divideAndConquer(postorder, l, end - 1);
+        return left && right;
+    }
 }
