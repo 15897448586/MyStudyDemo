12c94fe3d06fffd34c421f6c0544ee9495b3950b
BG270 Lixiong Zhang <BUGONGSOFT\BG270>
2024-05-06 18:06:58 +0800

    update 62

--- src/TargetOffer/Solution62.java
+++ src/TargetOffer/Solution62.java
@@ -8,14 +8,30 @@ import java.util.Arrays;
  */
 public class Solution62 {
     public int[] statisticalResult(int[] arrayA) {
+        int count0 = 0;
         int[] res = new int[arrayA.length];
         int sum = 1;
-        for (int i = 0; i < arrayA.length ; i++) {
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] == 0) {
+                if (count0 == 1) {
+                    return res;
+                }
+                count0++;
+                continue;
+            }
             sum *= arrayA[i];
         }
-        for (int i = 0; i < arrayA.length; i++) {
-            res[i] = sum / arrayA[i];
+        if (count0 == 0) {
+            for (int i = 0; i < arrayA.length; i++) {
+                res[i] = sum / arrayA[i];
+            }
+        } else {
+            for (int i = 0; i < arrayA.length; i++) {
+                if (arrayA[i] != 0) continue;
+                res[i] = sum;
+            }
         }
         return res;
     }
 }
+
